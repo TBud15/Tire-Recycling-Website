98e9038f0dfe18a69cf6e00342228451ed5b2f9c
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-05-07 20:11:31 -0400

    using div instead of section for correct page structure

--- src/app/[locale]/crumb-rubber/Contact/Contact.tsx
+++ src/app/[locale]/crumb-rubber/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 const Contact: React.FC = () => {
   return (
-    <section className="bg-white dark:bg-gray-900 mb-10">
+    <div className="bg-white dark:bg-gray-900 mb-10">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
@@ -26,7 +26,7 @@ const Contact: React.FC = () => {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
