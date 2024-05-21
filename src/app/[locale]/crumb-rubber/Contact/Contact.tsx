c230e735f7e755bd98a15053528f57c93bb7855f
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-05-20 20:23:48 -0400

    fixes for page change on click. Products pages.

--- src/app/[locale]/crumb-rubber/Contact/Contact.tsx
+++ src/app/[locale]/crumb-rubber/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import NavigationLink from "@/components/NavigationLink";
 
 const Contact: React.FC = () => {
   const t = useTranslations("CrumbRubberPage");
@@ -17,12 +18,12 @@ const Contact: React.FC = () => {
             {t("contact-description-two")}
           </p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-            <a
-              href="#" //Contact us page link
-              className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center  border  rounded-lg focus:ring-4 text-white border-gray-600 hover:bg-gray-700 focus:ring-gray-600"
+            <NavigationLink
+              href={"/contact-us" as any} //Contact us page link
+              className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center border rounded-lg focus:ring-4 text-white border-gray-600 hover:bg-gray-700 focus:ring-gray-600"
             >
               {t("contact-button")}
-            </a>
+            </NavigationLink>
           </div>
         </div>
       </div>
