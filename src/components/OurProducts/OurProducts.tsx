176b14ca0288cc7650d3e1866f945dc55184f8b2
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-09-04 23:46:22 -0400

    one more padding added

--- src/components/OurProducts/OurProducts.tsx
+++ src/components/OurProducts/OurProducts.tsx
@@ -13,7 +13,7 @@ export default function OurProducts() {
 
   return (
     <section className="bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-0 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">
             {t("title")}
