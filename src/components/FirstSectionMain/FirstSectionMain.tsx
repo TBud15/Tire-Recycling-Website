c3aec482a6b6d50066ec0f5ef1ce83d4cba90848
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-09-04 23:42:39 -0400

    changed padding to main page two upper sections

--- src/components/FirstSectionMain/FirstSectionMain.tsx
+++ src/components/FirstSectionMain/FirstSectionMain.tsx
@@ -7,7 +7,7 @@ export default function FirstSectionMain() {
 
   return (
     <section className=" bg-gray-900">
-      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-10 lg:px-6">
         <div className="font-light sm:text-lg text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">
             {t("title")}
